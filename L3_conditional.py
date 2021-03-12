1547e148aeb884d8db973d2ea85ab3d2ab41823a
hung.dao <daoquanghunggtvt@gmail.com>
2021-03-12 07:38:16 +0700

    L3_conditional

---
+++ L3_conditional.py
@@ -0,0 +1,29 @@
+# Bài 1:
+# Tiêu chí đánh giá học lực của một hoc sinh như sau:
+# Tổng điểm các môn từ 8 trở lên là hoc sinh giỏi
+# Tổng điểm các môn từ 6 cho đến 8 là hoc sinh khá
+# Tổng điểm các môn từ 4 cho đến 6 là trung bình
+# Dưới 4 là hoc sinh yếu.
+# Viết chương trình nhập tổng điểm hoc sinh từ màn hình và in ra kết quả "giỏi", "khá", "trung bình", "yếu"
+
+tong_diem_cac_mon = input("Vui lòng nhập tổng điểm các môn cho học sinh:")
+
+
+######################################################################################################################
+
+# Bài 2
+# Nhập độ dài ba cạnh từ màn hình, kiểm tra chúng có hợp lại thành một tam giác hay ko? Nếu có in câu "Tạo thành tam giác",
+# Nếu không, in câu "Không thể tao thành tam giác"
+
+######################################################################################################################
+
+# Bài 3
+# Giải phương trình căn bậc hai: a^2 + bx +c = 0; với a,b,c là các số nhập từ mành hình. Sau đó in ra kết quả tương ứng.
+# Notes: căn bậc hai là hàm "sqrt", cách sử dụng coi thêm https://www.geeksforgeeks.org/python-math-function-sqrt/
+
+######################################################################################################################
+
+# Bài 4
+# Viết chương trình kiểm tra chiều dài của 1 chuỗi nhập vào có chia hết cho 3 và 5 hay không? Nếu đứng, thì in "Great!", nếu ko
+# thay thế ký tự cuối cùng của chuỗi bằng "???????", và in ra màn hình
+chuoi_ky_tu = input("Nhap chuỗi ký tự")
